fade8bd6bb66591cefbeef97ec5ce6468f86b0b0
Matthias Buchholz <matthias@afkm.io>
2017-04-05 14:07:52 +0200

    target es5

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const cuid = require("cuid");
+var cuid = require("cuid");
 exports.defaultTag = {
     head: '{{',
     tail: '}}'
 };
-class CBMarker {
-    static CreateId(options = {}) {
-        const { useSlug = true } = options;
+var CBMarker = (function () {
+    function CBMarker() {
+    }
+    CBMarker.CreateId = function (options) {
+        if (options === void 0) { options = {}; }
+        var _a = options.useSlug, useSlug = _a === void 0 ? true : _a;
         return useSlug ? cuid.slug() : cuid();
-    }
-    static Create(options = {}) {
-        const { tag = exports.defaultTag, useSlug = true } = options;
-        const markerId = CBMarker.CreateId(options);
+    };
+    CBMarker.Create = function (options) {
+        if (options === void 0) { options = {}; }
+        var _a = options.tag, tag = _a === void 0 ? exports.defaultTag : _a, _b = options.useSlug, useSlug = _b === void 0 ? true : _b;
+        var markerId = CBMarker.CreateId(options);
         return [
-            `${tag.head}${markerId}${tag.tail}`,
-            `${tag.head}/${markerId}${tag.tail}`
+            "" + tag.head + markerId + tag.tail,
+            tag.head + "/" + markerId + tag.tail
         ];
-    }
-}
+    };
+    return CBMarker;
+}());
 exports.CBMarker = CBMarker;
 //# sourceMappingURL=index.js.map
